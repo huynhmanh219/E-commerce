2c8e2be7f0afe50bf7538c3f9e2a4a06d5aa7afb
huynhmanh219 <135082065+huynhmanh219@users.noreply.github.com>
2024-12-25 19:41:23 +0700

    tối ưu đoạn socket

--- controller/client/chat.controller.js
+++ controller/client/chat.controller.js
@@ -1,52 +1,15 @@
 const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model");
 const uploadToCloudinary = require('../../helper/uploadToCloudinary');
-
+const chatSocket = require("../../sockets/client/chat.socket");
 //[GET] /chat
 module.exports.index = async (req,res)=>{
-    const userId = res.locals.user.id;    
-    const fullname = res.locals.user.fullname;
-    //socket
-    _io.once("connection",(socket)=>{ // io.once chỉ load duy nhât một lần khác với io.on mỗi lần load trang là 1 làn load connection
-        socket.on("CLIENT_SEND_MESSAGE",async (data)=>{
-            let images = [];
-            
-            for (const imageBufer of data.images) {
-                const link = await uploadToCloudinary(imageBufer);
-                   
-                images.push(link);
-            }
-            console.log(data.images);
-            
-            // save in databe
-            const chat = new Chat({
-                user_id:userId,
-                content:data.content,
-                images:images
-            });
-            await chat.save();
 
-            // trả về data cho client
-            _io.emit("SERVER_RETURN_MESSAGE",{
-                userId:userId,
-                fullname:fullname,
-                content: data.content,
-                images:images
-            });
-        
-        }) ;
-        socket.on("CLIENT_SEND_TYPING",async(type)=>{
-                socket.broadcast.emit("SERVER_RETURN_TYPING",{
-                    userId:userId,
-                    fullname:fullname,
-                    type:type
-                })
-      
-            });
-      
-        
-    })
+    //socket
+    chatSocket(res);
     //end socket
+
+    
     //lấy data từ db 
     const chats = await Chat.find({
         deleted:false
---
+++ sockets/client/chat.socket.js
@@ -0,0 +1,47 @@
+const uploadToCloudinary = require('../../helper/uploadToCloudinary');
+const Chat = require("../../models/chat.model");
+
+
+module.exports = (res)=> {
+    const userId = res.locals.user.id;    
+    const fullname = res.locals.user.fullname;
+    _io.once("connection",(socket)=>{ // io.once chỉ load duy nhât một lần khác với io.on mỗi lần load trang là 1 làn load connection
+        socket.on("CLIENT_SEND_MESSAGE",async (data)=>{
+            let images = [];
+            
+            for (const imageBufer of data.images) {
+                const link = await uploadToCloudinary(imageBufer);
+                   
+                images.push(link);
+            }
+            console.log(data.images);
+            
+            // save in databe
+            const chat = new Chat({
+                user_id:userId,
+                content:data.content,
+                images:images
+            });
+            await chat.save();
+
+            // trả về data cho client
+            _io.emit("SERVER_RETURN_MESSAGE",{
+                userId:userId,
+                fullname:fullname,
+                content: data.content,
+                images:images
+            });
+        
+        }) ;
+        socket.on("CLIENT_SEND_TYPING",async(type)=>{
+                socket.broadcast.emit("SERVER_RETURN_TYPING",{
+                    userId:userId,
+                    fullname:fullname,
+                    type:type
+                })
+      
+            });
+      
+        
+    })
+}
